0a138947f0da1416b8c87dadd5b1fa5eaf76e011
october-rain <572626298@qq.com>
2022-03-20 16:49:15 +0800

    feat: 添加sass和css modules支持, 并修改首页背景图

---
+++ declaration.d.ts
@@ -0,0 +1,3 @@
+// 声明文件，避免下面代码报错
+// import styles from "./index.module.scss"; 
+declare module '*.scss';
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -33,7 +33,7 @@ const config = {
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve('./src/css/custom.scss'),
         },
       }),
     ],
@@ -113,6 +113,7 @@ const config = {
         darkTheme: darkCodeTheme,
       },
     }),
+  plugins: ['docusaurus-plugin-sass'],
 };
 
 module.exports = config;
